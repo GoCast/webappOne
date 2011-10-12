8e5bc0e173d46426a8557d406f1181a7e4bd6f11
Franz Pereira <frnz.cr@gmail.com>
2011-10-11 18:10:46 -0600

    Moving as a submodule

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeuronOne::Application.config.secret_token = 'd6ebec06c13a17c6e09fefedf57a9fc65fc8710d6a099b5671123c3652ef9a12f5606d0969ff8b631e6afe7d102c2b36c96dd2a89086cb4c831cf12a072e5470'
